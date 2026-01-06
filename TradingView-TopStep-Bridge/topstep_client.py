2616d9783cb52dd68f850f462d641dccf9032995
Dsavvi180 <127865498+Dsavvi180@users.noreply.github.com>
2026-01-06 13:41:06 +0000

    changed to live account

--- TradingView-TopStep-Bridge/topstep_client.py
+++ TradingView-TopStep-Bridge/topstep_client.py
@@ -9,7 +9,7 @@ load_dotenv(ENV_FILE)
 
 # Static Credentials
 TOPSTEP_USER = "quantsavvi"   
-ACCOUNT_ID = 16123578  
+ACCOUNT_ID = 16123563  
 CONTRACT_ID = "CON.F.US.MNQ.H26"
 DEFAULT_SIZE = 2
 TICK_SIZE = 0.25 # MNQ Tick Size
